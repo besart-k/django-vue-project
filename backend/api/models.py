01cfd47b7bbd3111538d113ed00642d747ff479f
BesartK <bkodraliu97@gmail.com>
2021-02-21 20:38:16 +0100

    created a  basic models for  risktypes  and riskdata

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    # TODO: Remove null=True and link with request user
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                                   null=True, related_name='%(app_label)s_%(class)s_created_by')
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                                   null=True, related_name='%(app_label)s_%(class)s_updated_by')
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class RiskTypeDefinition(BaseModel):
+    id = models.AutoField(db_column="Id", primary_key=True)
+    name = models.CharField(db_column="Name", max_length=50, unique=True)
+    definition = models.JSONField()
+
+    class Meta:
+        db_table = 'RiskTypesDefinitions'
+        managed = True
+        verbose_name = 'Risk Definition'
+        verbose_name_plural = 'Risk Definitions'
+
+    def __str__(self):
+        return self.name
+
+
+class RiskTypeData(BaseModel):
+    id = models.AutoField(db_column="Id", primary_key=True)
+    risk_type_definition = models.ForeignKey(
+        RiskTypeDefinition, on_delete=models.CASCADE)
+    data = models.JSONField()
+
+    class Meta:
+        db_table = 'RiskTypeData'
+        managed = True
+        verbose_name = 'Risk Type Data'
+        verbose_name_plural = 'Risk Types Data'
+
+    def __str__(self):
+        return str(self.id)
