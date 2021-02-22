feef69eba1e275c77d676d772638c9f713f7c044
BesartK <bkodraliu97@gmail.com>
2021-02-22 23:56:05 +0100

    added simple json scheema validatior

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.conf import settings
+from jsonschema import validate
 
 
 class BaseModel(models.Model):
-    # TODO: Remove null=True and link with request user
+    # TODO: Remove null=True and link with request user if needed
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-                                   null=True, related_name='%(app_label)s_%(class)s_created_by')
+                                   null=True, blank=True,related_name='%(app_label)s_%(class)s_created_by')
     updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-                                   null=True, related_name='%(app_label)s_%(class)s_updated_by')
+                                   null=True, blank=True,related_name='%(app_label)s_%(class)s_updated_by')
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
@@ -44,3 +45,7 @@ class RiskTypeData(BaseModel):
 
     def __str__(self):
         return str(self.id)
+    
+    def clean(self):
+        
+        validate(instance=self.data, schema=self.risk_type_definition.definition)
