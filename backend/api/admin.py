e9158838a7a64e9d19131d0ba4d8b2f21f19ac4a
BesartK <bkodraliu97@gmail.com>
2021-02-22 23:54:35 +0100

    registred RiskTypeData and RiskTypeDefinition to django admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from api.models import RiskTypeDefinition, RiskTypeData
 
-# Register your models here.
+admin.site.register(RiskTypeData)
+admin.site.register(RiskTypeDefinition)
