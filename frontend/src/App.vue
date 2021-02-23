788f822e399fd14e3065071a58f785d3fc5d225e
BesartK <bkodraliu97@gmail.com>
2021-02-23 22:55:34 +0100

    implemented simple vue view to show RiskTypes from rest api

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>List of Risk Types</h1>
+    </header>
+    <main>
+      <aside class="sidebar">
+        <div v-for="type in riskTypes" v-bind:key="type.id">
+          {{ type.name }}
+        </div>
+      </aside>
+      <div class="content">
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import axios from 'axios'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data() {
+      return {
+        riskTypes: [],
+        endpoint: 'http://localhost:8000/risk-type-definitions/'
+      }
+    },
+    created() {
+      this.getAllRiskTypes();
+    },
+
+    methods: {
+      getAllRiskTypes() {
+        axios.get(this.endpoint).then(response => {
+          this.riskTypes = response.data;
+        }).catch(error => {
+          console.log('-----error-------');
+          console.log(error);
+        })
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ frontend/src/components/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data(){
+    return {
+      msg:'Welcome home!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
