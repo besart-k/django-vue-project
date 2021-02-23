6a5c08a2c3adb318aa3f7ce5588d85851207200a
BesartK <bkodraliu97@gmail.com>
2021-02-23 21:58:28 +0100

    disabled destroy update and partial_update methods

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 
 from api.serializers import RiskTypeDefinitionSerializer, RiskTypeDataSerializer
 from api.models import RiskTypeDefinition, RiskTypeData
@@ -8,7 +8,31 @@ class RiskTypeDefinitionViewSet(viewsets.ModelViewSet):
     queryset = RiskTypeDefinition.objects.all()
     serializer_class = RiskTypeDefinitionSerializer
 
+    def destroy(self, request, pk=None):
+        response = {'message': 'Delete function is not offered in this path.'}
+        return Response(response, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def update(self, request, pk=None):
+        response = {'message': 'Update function is not offered in this path.'}
+        return Response(response, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def partial_update(self, request, pk=None):
+        response = {'message': 'Update function is not offered in this path.'}
+        return Response(response, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
 
 class RiskTypeDataViewSet(viewsets.ModelViewSet):
     queryset = RiskTypeData.objects.all()
     serializer_class = RiskTypeDataSerializer
+
+    def destroy(self, request, pk=None):
+        response = {'message': 'Delete function is not offered in this path.'}
+        return Response(response, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def update(self, request, pk=None):
+        response = {'message': 'Update function is not offered in this path.'}
+        return Response(response, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def partial_update(self, request, pk=None):
+        response = {'message': 'Update function is not offered in this path.'}
+        return Response(response, status=status.HTTP_405_METHOD_NOT_ALLOWED)
